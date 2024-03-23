90b5acde0a28b0cfaeee6733e611fb56ddc2467b
osama.aldalati <osama.aldalati@ivfp.de>
2024-03-23 23:57:30 +0100

    struktur anpassen

---
+++ todo-cli.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import { create_commands } from "./src/utils/create_commands";
+
+const args = process.argv.slice(2);
+
+const commands = create_commands(args);
+
+commands.forEach(command => command.run());
+
+process.exit();
